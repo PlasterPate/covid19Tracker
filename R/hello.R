699f8d3c3e6e732dffbdc7dc0d096a81c0eefb34
Ali Mohammad Movahedian <am.movahedian1998@gmail.com>
2020-10-28 17:46:45 +0330

    Initial commit

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,6 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+usethis::use_git()
